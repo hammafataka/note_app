baa825b2e9d5df69722a57a404a12ab81133a20e
hammafataka <66084806+hammafataka@users.noreply.github.com>
2021-03-11 09:45:23 +0100

    Creating save and share Button

--- note_app/note_app/NoteDetailsPage.xaml.cs
+++ note_app/note_app/NoteDetailsPage.xaml.cs
@@ -7,21 +7,32 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using System.IO;
 
 namespace note_app
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class NoteDetailsPage : ContentPage
     {
+        string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+        
+
+
         NotePage page2;
         public NoteDetailsPage(NotePage page)
         {
             InitializeComponent();
             page2 = page;
-           
 
+            BindingContext = new Note();
+            if (File.Exists(_fileName))
+            {
+                editor2.Text = File.ReadAllText(_fileName);
+            }
 
         }
+        
         protected override void OnAppearing()
         {
             base.OnAppearing();
@@ -37,6 +48,23 @@ namespace note_app
 
             }
             editor2.Text = page2.details;
+
+
+        }
+
+        private async void shareBtn_Clicked(object sender, EventArgs e)
+        {
+            await Share.RequestAsync(new ShareTextRequest
+            {
+                Text = editor2.Text,
+                Title="share"
+            }
+            ) ;
+        }
+
+        private  void saveBtn_Clicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(_fileName, editor2.Text);
 
 
         }
